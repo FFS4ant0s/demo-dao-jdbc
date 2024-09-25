eb8d31a846f0ff7234ed2c11ef8f1e7488f68ebf
Fernando <silentguyband@gmail.com>
2024-09-25 13:42:48 -0300

    update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,7 +39,12 @@ public class Program {
 		Seller newseller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newseller);
 		System.out.println("Inserted! new id = " + newseller.getId());
-
+		
+		System.out.println("\n=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
